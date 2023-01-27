617e40b9d4461838c0f75b1053903eb744044917
gustavobaltazar <gustavobaltazar31@gmail.com>
2023-01-27 08:34:53 -0300

    feat: setup client side
    - setup tailwind & react query

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { api } from "./utils/trpc";
+
+
+function App() {
+  return (
+    <div className="text-2xl">
+      oi
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/utils/trpc.ts
@@ -0,0 +1,15 @@
+import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+import { inferRouterInputs } from '@trpc/server';
+import superjson from 'superjson';
+import { AppRouter } from '../../../server/src/server';
+
+export const api = createTRPCProxyClient<AppRouter>({
+    transformer: superjson,
+    links: [
+        httpBatchLink({
+            url: 'http://localhost:4000/trpc',
+        }),
+    ],
+});
+
+export type RouterInputs = inferRouterInputs<AppRouter>;
