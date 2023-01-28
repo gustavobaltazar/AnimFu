04f4e8f91f1036f94c2ca93a5cb01e225e63f09e
gustavobaltazar <gustavobaltazar31@gmail.com>
2023-01-28 18:17:09 -0300

    feat(auth): logout user & clear cookie

--- client/src/tests/auth.tsx
+++ client/src/tests/auth.tsx
@@ -1,42 +1,83 @@
-import { useState } from "react"
-import { useMutation } from "react-query"
-import { api } from "../utils/trpc"
+import { useState } from "react";
+import { useMutation } from "react-query";
+import { api } from "../utils/trpc";
 
 export default function AuthPage() {
-    const createUser = useMutation(["createUser"], api.user.createUser.mutate)
-    const loginUser = useMutation(["loginUser"], api.user.loginUser.mutate)
-    const [name, setName] = useState("")
-    const [age, setAge] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    return (
-        <div>
-            <h1>Auth</h1>
-            <div>
-                <h3>Register user</h3>
-                <form onSubmit={(e) => {
-                    e.preventDefault()
-                    createUser.mutate({ name, age: Number(age), email, password })
-                }}>
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
-                    <input type="number" value={age} onChange={(e) => setAge(e.target.value)} placeholder="Age" />
-                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-                    <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-                    <button type="submit">Create user</button>
-                </form>
-            </div>
-            <div>
-                <h3>Log in user</h3>
-                <form onSubmit={(e) => {
-                    e.preventDefault()
-                    loginUser.mutate({ email, password })
-                }}>                    
-                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-                    <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-                    <button type="submit">Login</button>
-                </form>
-            </div>
-
-        </div>
-    )
+  const createUser = useMutation(["createUser"], api.user.createUser.mutate);
+  const loginUser = useMutation(["loginUser"], api.user.loginUser.mutate);
+  const logoutUser = useMutation(["logoutUser"], api.user.logoutUser.mutate);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const handleLogout = async () => {
+    logoutUser.mutate()
+  }
+  return (
+    <div>
+      <h1>Auth</h1>
+      <div>
+        <h3>Register user</h3>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            createUser.mutate({ name, age: Number(age), email, password });
+          }}
+        >
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Name"
+          />
+          <input
+            type="number"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+            placeholder="Age"
+          />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+          <input
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <button type="submit">Create user</button>
+        </form>
+      </div>
+      <div>
+        <h3>Log in user</h3>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            loginUser.mutate({ email, password });
+          }}
+        >
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+          <input
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+      <div>
+        <button onClick={handleLogout}>logout</button>
+      </div>
+    </div>
+  );
 }
