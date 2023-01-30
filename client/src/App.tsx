b5220da7d2f1f3645fddc6c640a0f80bf15fb089
gustavobaltazar <gustavobaltazar31@gmail.com>
2023-01-30 00:48:20 -0300

    feat(Login): frontend loginPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,22 @@ import { useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import { api } from "./utils/trpc";
 
-
 function App() {
   const queryClient = useQueryClient();
-  const animeQuery = useQuery(['getAllAnimes'], () => api.anime.getAllAnimes.query())
-  const createAnimeMutation = useMutation(['createAnime'], api.anime.createAnime.mutate)
+  const animeQuery = useQuery(["getAllAnimes"], () =>
+    api.anime.getAllAnimes.query()
+  );
+  const createAnimeMutation = useMutation(
+    ["createAnime"],
+    api.anime.createAnime.mutate
+  );
 
-  const [genre, setGenre] = useState("")
-  const [name, setName] = useState("")
-  const [score, setScore] = useState(0)
-  const [season, setSeason] = useState("")
-  const [synopsis, setSynopsis] = useState("")
-  const [year, setYear] = useState("")
+  const [genre, setGenre] = useState("");
+  const [name, setName] = useState("");
+  const [score, setScore] = useState(0);
+  const [season, setSeason] = useState("");
+  const [synopsis, setSynopsis] = useState("");
+  const [year, setYear] = useState("");
 
   if (animeQuery.isLoading) return <div>Loading...</div>;
   if (animeQuery.isError) return <div>ERROR!</div>;
@@ -22,21 +26,61 @@ function App() {
     <div className="text-2xl">
       oi
       <p>{JSON.stringify(animeQuery.data?.allAnimes)}</p>
-      <form onSubmit={(e) => {
-        e.preventDefault()
-        createAnimeMutation.mutate({ genre, name, score, season, synopsis, year });
-        queryClient.invalidateQueries('getAllAnimes');
-      }}>
-        <input type="text" placeholder="Genre" value={genre} onChange={(e) => setGenre(e.target.value)} />
-        <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-        <input type="number" placeholder="Score" value={score} onChange={(e) => setScore(e.target.valueAsNumber)} />
-        <input type="text" placeholder="Season" value={season} onChange={(e) => setSeason(e.target.value)} />
-        <input type="text" placeholder="Synopsis" value={synopsis} onChange={(e) => setSynopsis(e.target.value)} />
-        <input type="text" placeholder="Year" value={year}  onChange={(e) => setYear(e.target.value)} />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          createAnimeMutation.mutate({
+            genre,
+            name,
+            score,
+            season,
+            synopsis,
+            year,
+          });
+          queryClient.invalidateQueries("getAllAnimes");
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Genre"
+          value={genre}
+          onChange={(e) => setGenre(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Score"
+          value={score}
+          onChange={(e) => setScore(e.target.valueAsNumber)}
+        />
+        <input
+          type="text"
+          placeholder="Season"
+          value={season}
+          onChange={(e) => setSeason(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Synopsis"
+          value={synopsis}
+          onChange={(e) => setSynopsis(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Year"
+          value={year}
+          onChange={(e) => setYear(e.target.value)}
+        />
         <button>Add anime</button>
       </form>
+      <div>aaaaaaaaa</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import AuthPage from './tests/auth'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import AuthPage from "./tests/auth";
+import { Login } from "./pages/Login";
 
-const queryClient = new QueryClient()
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
-      <AuthPage />
+      {/* <App />
+      <AuthPage /> */}
+      <Login />
     </QueryClientProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
