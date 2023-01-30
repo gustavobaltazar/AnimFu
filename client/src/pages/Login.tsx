bd4a385db054b7731ecd58ab0a58f24b1883bde0
gustavobaltazar <gustavobaltazar31@gmail.com>
2023-01-30 16:08:04 -0300

    feat(register): register page connected with backend
    - login changed how button works

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -15,13 +15,13 @@ export function Login() {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <form className="space-y-6"
+        <form
+          className="space-y-6"
           onSubmit={(e) => {
             e.preventDefault();
             loginUser.mutate({ email, password });
           }}
         >
-
           <h5 className="text-xl font-medium text-gray-900 dark:text-white text-center">
             Login
           </h5>
@@ -93,13 +93,7 @@ export function Login() {
           >
             Login
           </button>
-          <button
-            type="submit"
-            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            onClick={handleLogout}
-          >
-            Logout
-          </button>
+
           <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
             Not registered?{" "}
             <a
@@ -110,6 +104,15 @@ export function Login() {
             </a>
           </div>
         </form>
+        <div>
+          <button
+            type="submit"
+            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
       </div>
     </div>
   );
